3313d5072a738d2d7bb336fb498ac52affb69cae
erin johnson <erin.nicole.johnson@gmail.com>
2019-05-20 20:00:55 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -26,5 +26,5 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  batch_badge_creator(attendees).each { |a, i| puts "#{a} is at position #{i}" }
+  batch_badge_creator(attendees).each { |a| puts "#{a}" }
 end
